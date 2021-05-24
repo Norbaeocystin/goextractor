6eb142a4a55f0c151be14d8fe5682469b1b9d398
Norbaeocystin <baranrastislav@gmail.com>
2021-05-24 22:12:12 +0200

    added function

--- pkg/goextractor/extractor.go
+++ pkg/goextractor/extractor.go
@@ -1,1 +1,14 @@
 package goextractor
+
+// Get Data Via Using chrome
+func GetDataViaChrome(urlstring string) map[string]interface{} {
+	s, _ := GetByChrome(urlstring)
+	doc, _ := GetDoc([]byte(s))
+	results := Extract(doc, Ids2name)
+	data := make(map[string]interface{})
+	data["source"] = urlstring
+	for k, v := range results {
+		data[k] = v
+	}
+	return data
+}
